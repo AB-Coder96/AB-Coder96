f200043876865cd665dcb1df1a40e5b52765f407
Araz Bagherzadeh Karimi <arazbagherzadeh@gmail.com>
2024-05-18 10:36:04 -0400

    manual web updates (ArazKarimiDesktop)

--- Push.bat
+++ Push.bat
@@ -1,6 +1,10 @@
 python ExceltoWord.py
 python WordtoHTML.py
+setlocal enabledelayedexpansion
+for /f "tokens=*" %%a in ('hostname') do set "computer_name=%%a"
+git submodule foreach 'git stash'
 Git add .
-Git commit -m" Automatic Website deployment"
+Git commit -m "manual web updates (!computer_name!)"
 Git push
+endlocal
 pause
